a75e6a2bbbfc88719586c01db519eefdf7789784
gouj991017 <60113080+gouj991017@users.noreply.github.com>
2022-07-11 19:08:00 -0400

    update

--- problematique/main.py
+++ problematique/main.py
@@ -12,11 +12,14 @@ signal1, Fs1 = sf.read('note_guitare_LAd.wav')
 signal2, Fs2 = sf.read('note_basson_plus_sinus_1000_Hz.wav')
 N1 = len(signal1)
 N2 = len(signal2)
+#Variables de l'Enveloppe Temporelle et de son filtre Passe-Bas
 K = 885
 Fc = np.pi/1000
 w = 2*np.pi*Fc
-print(signal1)
-print(signal2)
+print(Fs2)
+print(N1)
+#Variables du Filtre Coupe-Bande
+N = 1024
 
 x = np.fft.fft(signal1)
 y = np.fft.fft(signal2)
@@ -34,38 +37,75 @@ y = np.fft.fft(signal2)
 peaks1 = np.asarray(find_peaks(x, distance=1000))[0]
 peaks2 = np.asarray(find_peaks(y, distance=600))[0]
 
-#Conversion des 32 premiers INDEX en Hz
-freq_peaks1 = (peaks1[1:33]/N1)*Fs1
-freq_peaks2 = (peaks2[1:33]/N2)*Fs2
+#Conversion des INDEX en Hz
+freq_peaks1 = np.fft.fftfreq(N1, d=(1/Fs1)) #(peaks1[1:33]/N1)*Fs1
+freq_peaks2 = np.fft.fftfreq(N2, d=(1/Fs2))
+Frequences = freq_peaks1[peaks1[1:33]]
+
 #Phase (J'ai des doutes à savoir quel méthode est véridict)
-xphase = np.angle(peaks1[1:33]) #peaks1[1:33]/np.abs(peaks1[1:33])
+xphase = np.angle(x) #peaks1[1:33]/np.abs(peaks1[1:33])
 yphase = np.angle(peaks2[1:33]) #peaks2[1:33]/np.abs(peaks2[1:33])
+Phase = xphase[peaks1[1:33]]
+
 #Amplitude
-amp_signal1 = np.abs(peaks1[1:33])
+amp_signal1 = np.abs(x) #np.abs(peaks1[1:33])
 amp_signal2 = np.abs(peaks2[1:33])
+Amplitude = amp_signal1[peaks1[1:33]]
 
-#plt.plot(xphase)
-#plt.plot(yphase)
-#plt.figure()
-#plt.plot(amp_signal1)
-#plt.plot(amp_signal2)
-
-w1 = 2*np.pi*peaks1[1:33]
-w2 = 2*np.pi*peaks2[1:33]
 #Enveloppe Temporelle
-EnvTemp1 = (np.sin(w*K/2)/np.sin(w/2))/K
+EnvTemp1 = np.convolve(amp_signal1, np.ones(K)/K)
+#EnvTemp1 = (np.sin(w*K/2)/np.sin(w/2))/K
 EnvTemp2 = (np.sin(w*K/2)/np.sin(w/2))/K
 
+print(Frequences)
+plt.plot(20*np.log10(amp_signal1))
+print(Amplitude)
+print(Phase)
 #Filtre
-Filtre_bp = ((np.sin(5*np.pi*n/32)/np.sin(w/2))/K/16)*np.cos(5*np.pi/8*n)
+n = np.arange(N1)
+K_cp = 3
+Filtre_cp = ((np.sin(K_cp*np.pi*n/N)/np.sin(np.pi*n/N))/(N/2))*np.cos(K_cp*np.pi/8*n)
+
+#Transformation des notes de musique
+Freq_SOL = 0.841*Frequences
+Freq_MI = 0.707*Frequences
+Freq_FA = 0.749*Frequences
+Freq_RE = 0.630*Frequences
 
 #Compilation des sons
-Son_Guitar = amp_signal1*np.sin(w1 + xphase)*EnvTemp1
-Son_Basson = amp_signal2*np.sin(w2 + yphase)*EnvTemp2
-plt.plot(EnvTemp1)
-print(xphase)
+i = 1
+Somme_amp_signal1 = 0
+Somme_xphase = 0
+Somme_Sinus = Amplitude[0] * np.sin(2 * np.pi * Frequences[0] * (n / Fs1) + Phase[0])
+while i < 32:
+    Somme_Sinus = Somme_Sinus + Amplitude[i] * np.sin(2 * np.pi * Frequences[i] * (n / Fs1) + Phase[i])
+    Somme_amp_signal1 = Somme_amp_signal1 + amp_signal1[i]
+    Somme_xphase = Somme_xphase + xphase[i]
+    i = i +1
+print(Somme_amp_signal1)
+print(Somme_xphase)
+Son_Guitar = np.zeros(N1)
+j = 0
+#while j < N1:
+#    w1 = 2 * np.pi * (j/N1)*Fs1
+#    w2 = 2 * np.pi * freq_peaks2
+#    Son_Guitar[j] = Somme_amp_signal1 * np.sin(w1 * (j / Fs1) + Somme_xphase)  # *EnvTemp1[0:32]
+#    j = j+1
+Son_Guitar = Somme_Sinus*(1/1000)*EnvTemp1[0:N1]
+#Son_Guitar = Somme_amp_signal1*np.sin(w1*(n/44100) + Somme_xphase)#*EnvTemp1[0:32]
+#Son_Basson = amp_signal2*np.sin(w2 + yphase)*EnvTemp2
+
+#print(xphase)
 print(amp_signal1)
-print(EnvTemp1)
+#print(EnvTemp1)
+#plt.plot(signal1)
+plt.figure()
+plt.plot(Son_Guitar)
+#plt.figure()
+#plt.plot(EnvTemp1)
+#plt.figure()
+#plt.plot(x[peaks1[1:33]])
+#print(x[peaks1[1:33]])
 
 #plt.title("Fast Fourier transform")
 #plt.xlabel("Frequency")
@@ -79,7 +119,7 @@ print(EnvTemp1)
 #plt.plot(xphase)
 plt.show()
 print(Son_Guitar)
-sf.write('son_synth_guitar.wav', Son_Guitar, samplerate=len(Son_Guitar))
+sf.write('son_synth_guitar.wav', Son_Guitar, samplerate=Fs1)
 #sf.write('son_filtre_basson.wav', Son_Basson, samplerate=len(Son_Basson))
 
 #plt.show()
